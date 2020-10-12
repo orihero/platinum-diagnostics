1be57e9c79a3a325c1d916f512ce2264a707695d
orihero <aka.orihero@gmail.com>
2020-10-12 17:13:04 +0000

    My changes

---
+++ build/precache-manifest.a577e204c41f5ac1456e934a8f954765.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b426cf4bceb45b3a28f618add79693f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0b2ec1c34ecd7cb4eab",
+    "url": "/static/css/2.40c68a69.chunk.css"
+  },
+  {
+    "revision": "8f361daa336924d1f3bf",
+    "url": "/static/css/main.359fd6df.chunk.css"
+  },
+  {
+    "revision": "a0b2ec1c34ecd7cb4eab",
+    "url": "/static/js/2.5ca97318.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5ca97318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f361daa336924d1f3bf",
+    "url": "/static/js/main.d3ea5226.chunk.js"
+  },
+  {
+    "revision": "f161e9d66dacd6f005a4",
+    "url": "/static/js/runtime-main.1ae4a865.js"
+  },
+  {
+    "revision": "cd2ae07145c667ad444b6002102be6df",
+    "url": "/static/media/call.cd2ae071.png"
+  },
+  {
+    "revision": "8c0df8236ee93e10cbc571373056d339",
+    "url": "/static/media/card-1.8c0df823.png"
+  },
+  {
+    "revision": "d71dbfc279c535d6fa46ef0a449cd2d3",
+    "url": "/static/media/card-2.d71dbfc2.png"
+  },
+  {
+    "revision": "a644522d776a7b3e59bc8349370e2177",
+    "url": "/static/media/card-3.a644522d.png"
+  },
+  {
+    "revision": "2a010a7d250a20b92a4324f6a7df5c27",
+    "url": "/static/media/card-4.2a010a7d.png"
+  },
+  {
+    "revision": "96fe3ae7a49ba700f82078233515c237",
+    "url": "/static/media/card-5.96fe3ae7.png"
+  },
+  {
+    "revision": "bc55725d88ee9462e80cac7baadd56ba",
+    "url": "/static/media/card-6.bc55725d.png"
+  },
+  {
+    "revision": "307330b1213b1b444ed629a9f1de57a3",
+    "url": "/static/media/card-7.307330b1.png"
+  },
+  {
+    "revision": "6a84fd83ba17122f09d51bed597c0bd9",
+    "url": "/static/media/card-8.6a84fd83.png"
+  },
+  {
+    "revision": "25e2c1d971eb5021b52f9ceca40c95a9",
+    "url": "/static/media/imgNews.25e2c1d9.jpg"
+  },
+  {
+    "revision": "2456799bcad3fc7c20c6a99cf2f06ba4",
+    "url": "/static/media/lab1.2456799b.jpg"
+  },
+  {
+    "revision": "b51cdae3c68a88179a143e09403a81d1",
+    "url": "/static/media/lab2.b51cdae3.jpg"
+  },
+  {
+    "revision": "1d74f0226619a45f97529b3adacec6a2",
+    "url": "/static/media/lab3.1d74f022.jpg"
+  },
+  {
+    "revision": "1c10fd7ec7195ec52b157954dae4b4a4",
+    "url": "/static/media/lab4.1c10fd7e.jpg"
+  },
+  {
+    "revision": "381da975322cb0169454ec3f027cf273",
+    "url": "/static/media/lab5.381da975.jpg"
+  },
+  {
+    "revision": "35a63549cfa0b3970e1a5c7ad0036b3e",
+    "url": "/static/media/lab6.35a63549.jpg"
+  },
+  {
+    "revision": "7a78d98cbb5f09402bc921d26e87514a",
+    "url": "/static/media/lab7.7a78d98c.jpg"
+  }
+]);
